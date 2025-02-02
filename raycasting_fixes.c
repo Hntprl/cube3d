ca62272f704fe3ca7e833722cbaeb0cb7dad8aa5
bbenjrai <bbenjrai@c4r10p3.1337.ma>
2025-02-02 17:51:14 +0100

    frst step of textures 2

--- raycasting_fixes.c
+++ raycasting_fixes.c
@@ -6,7 +6,7 @@
 /*   By: bbenjrai <bbenjrai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 08:21:31 by amarouf           #+#    #+#             */
-/*   Updated: 2025/02/02 00:28:17 by bbenjrai         ###   ########.fr       */
+/*   Updated: 2025/02/02 17:50:56 by bbenjrai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,7 +52,60 @@ void	fix(int *x, int *y, t_mlx *mlx)
 	if (*y > HTH)
 		*y = HTH;
 }
+void	bresenham(t_mlx *mlx, t_wall wall,int texture_index)
+{
+	t_bnham bnham;
+	double	texture_step;
+	double	texture_pos;
+	int		color;
+	int		texture_x, texture_y ,wall_hit_pos;
+	
+	if (mlx->ray->was_hit_vertical)
+    wall_hit_pos = fmod(mlx->ray->y_hit, mlx->map->block_size) / mlx->map->block_size;
+	else
+    wall_hit_pos = fmod(mlx->ray->x_hit, mlx->map->block_size) / mlx->map->block_size;
+	bnham.dx = abs(wall.x2 - mlx->p->x);
+	bnham.dy = abs(wall.y2 - mlx->p->y);
+	texture_x = (int)(wall_hit_pos * mlx->texture[texture_index].t_width);
+	texture_step = (double)mlx->texture[texture_index].t_height / (wall.y2 - wall.y);
+	texture_pos = 0;
+	
+	if (wall.x == wall.x2)
+		bnham.sx = 0;
+	else if (wall.x < wall.x2)
+		bnham.sx = 1;
+	else
+		bnham.sx = -1;
+	if (wall.y == wall.y2)
+		bnham.sy = 0;
+	else if (wall.y < wall.y2)
+		bnham.sy = 1;
+	else
+		bnham.sy = -1;
+	bnham.error = bnham.dx - bnham.dy;
+	while (wall.x != wall.x2 || wall.y != wall.y2)
+	{
 
+		texture_y = (int)texture_pos % mlx->texture[texture_index].t_height;
+		texture_pos += texture_step;
+
+		// Fetch color from texture
+		color = *(int *)(mlx->addr->addr_e + (texture_y * mlx->addr->e_size_line) +
+			(texture_x * (mlx->addr->e_bits_per_pixel / 8)));
+		put_pixel(mlx->addr, wall.x, wall.y, color);//  here i need to render the textures on the walls 
+		bnham.e2 = bnham.error * 2;
+		if (bnham.e2 > -bnham.dy)
+		{
+			bnham.error -= bnham.dy;
+			wall.x += bnham.sx;
+		}
+		if (bnham.e2 < bnham.dx)
+		{
+			bnham.error += bnham.dx;
+			wall.y += bnham.sy;
+		}
+	}
+}
 
 
 void	draw_wall(t_mlx *mlx, int index)
